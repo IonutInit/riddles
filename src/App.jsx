e766672650f43aad6bb226a3aa62ebc6196ed7f3
IonutInit <ionutcojocaru@outlook.com>
2022-12-14 17:46:41 +0000

    connected to frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import RandomRiddle from "./RandomRiddle";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <RandomRiddle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RandomRiddle.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import {
+  checkSetSimilarity,
+  checkSynonymSimilarity,
+} from "./game-logic/checkSimilarity";
+import placeholder from "./assets/placeholder.jpg";
+
+const RandomRiddle = () => {
+  const [randomRiddle, setRandomRiddle] = useState("");
+  const [riddleSolution, setRiddleSolution] = useState("");
+  const [solutionSynonyms, setSolutionSynonyms] = useState("");
+  const [riddleImage, setRiddleImage] = useState("");
+
+  const [result, setResult] = useState("");
+  const [synonymResult, setSynonymResult] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  //gets random riddle
+  const getRandomRiddle = () => {
+    async function randomRiddle() {
+      setIsLoading(true);
+
+      const response = await fetch(
+        "https://the-path-of-riddles.onrender.com/openai/riddle-img",
+        {
+          method: "POST",
+        }
+      );
+      const data = await response.json();
+      setRandomRiddle(data.riddle[0].riddle);
+      setRiddleSolution(data.riddle[0].solution);
+      setSolutionSynonyms(data.riddle[0].synonyms);
+      setRiddleImage(data.imgUrl.url);
+
+      setIsLoading(false);
+    }
+    randomRiddle();
+  };
+
+  //checks similarity of both input and synonyms
+  const handleResult = (e) => {
+    const synonymArr = solutionSynonyms.split(" ");
+    setResult(checkSetSimilarity(e.target.value, riddleSolution));
+    setSynonymResult(checkSynonymSimilarity(e.target.value, synonymArr));
+  };
+
+  return (
+    <div>
+      <div>
+        <img
+          src={!riddleImage ? placeholder : riddleImage}
+          alt="riddle representation"
+        ></img>
+      </div>
+
+      <h3>{randomRiddle}</h3>
+      {/* <h5>{riddleSolution}</h5>
+      <p>{solutionSynonyms}</p> */}
+
+      <input type="text" onChange={(e) => handleResult(e)}></input>
+      <button onClick={() => getRandomRiddle()}>
+        {isLoading ? "Loading..." : "Get riddle"}
+      </button>
+
+      {result !== "" && <h3>{result}</h3> && synonymResult !== "" && (
+        <h3>{synonymResult}</h3>
+      )}
+    </div>
+  );
+};
+
+export default RandomRiddle;
