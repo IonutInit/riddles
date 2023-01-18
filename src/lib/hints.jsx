32d015cbfc6d420e6730187b40dc72c6468741aa
IonutInit <ionutcojocaru@outlook.com>
2023-01-18 11:30:03 +0000

    some tweaks to hints

--- src/lib/hints.jsx
+++ src/lib/hints.jsx
@@ -68,12 +68,44 @@ export const randomLetter = (word, synonymsString) => {
   return `One of the letters is ${word[rand].toUpperCase()}.`;
 };
 
+//Example of random letters without duplication, but cannot handle uni-directional data flow
+// let usedLetters = [];
+// const randomLetter = (word) => {
+//     let [...letterSet] = new Set(word);
+//     let result;
+//     if (usedLetters.length === letterSet.length) {
+//         return 'No more letters.'
+//     }
+//     do {
+//         result = letterSet[Math.floor(Math.random() * letterSet.length)].toUpperCase()
+//     } while (usedLetters.includes(result))
+//     usedLetters.push(result)
+//     return `One of the letters is ${result}.`
+// }
+
 export const synonym = (word, synonymsString) => {
   let synonyms = synonymsString.split(",");
   const rand = Math.floor(Math.random() * synonyms.length);
 
   return `Another word for it could be ${synonyms[rand].replace(" ", "")}.`;
 };
+
+//Example of synonyms without duplication, but cannot handle uni-directional data flow
+// let usedSynonymsArray = []
+// const synonym = (synonymString, usedSynonyms) => {
+//     usedSynonyms = usedSynonymsArray
+//     const synonyms = synonymString.split(',');
+//     let result;
+//     if (usedSynonyms.length === synonyms.length) {
+//         return 'No more synonyms.'
+//     }
+//     do {
+//         result = synonyms[Math.floor(Math.random() * synonyms.length)].trim()
+//     } while (usedSynonyms.includes(result));
+//     usedSynonyms.push(result)
+//     console.log(usedSynonymsArray)
+//     return `Another word for it might be ${result}.`
+// }
 
 export const firstLetter = (word, synonymsString) => {
   return `The first letter of the solution is ${word[0].toUpperCase()}.`;
@@ -83,119 +115,3 @@ export const endLetter = (word, synonymsString) => {
   return `The solution ends in ${word[word.length - 1].toUpperCase()}.`;
 };
 
-// let hints = [
-//   {
-//       call: averageLetter,
-//       status: null,
-//       points: 0, //how many times has been played this turn
-//       limit: 1, //how many times can be played by turn
-//       lowerLimit: 1, //it is available each turn within the lower/upper limits of the game
-//       upperLimit: 4,
-//       chance: 2, //the chance of it being called out of the summ of all chances
-//   },
-//   {
-//       call: approximateLengthHarder,
-//       status: null,
-//       points: 0,
-//       limit: 1,
-//       lowerLimit: 1,
-//       upperLimit: 5,
-//       chance: 2,
-//   },
-//   {
-//       call: approximateLengthEasier,
-//       status: null,
-//       points: 0,
-//       limit: 1,
-//       lowerLimit: 6,
-//       upperLimit: 10,
-//       chance: 1,
-//   },
-//   {
-//       call: approximateLengthEasiest,
-//       status: null,
-//       points: 0,
-//       limit: 1,
-//       lowerLimit: 11,
-//       upperLimit: 200,
-//       chance: 1,
-//   },
-//   {
-//       call: randomLetter,
-//       status: null,
-//       points: 0,
-//       limit: 3,
-//       lowerLimit: 1,
-//       upperLimit: 200,
-//       chance: 1,
-//   },
-//   {
-//       call: synonym,
-//       status: null,
-//       points: 0,
-//       limit: 2,
-//       lowerLimit: 1,
-//       upperLimit: 200,
-//       chance: 1,
-//   },
-//   {
-//       call: endLetter,
-//       status: null,
-//       points: 0,
-//       limit: 1,
-//       lowerLimit: 3,
-//       upperLimit: 200,
-//       chance: 1,
-//   },
-//   {
-//       call: firstLetter,
-//       status: null,
-//       points: 0,
-//       limit: 1,
-//       lowerLimit: 5,
-//       upperLimit: 200,
-//       chance: 2,
-//   },
-// ]
-
-const getHints = (solution, synonymsString) => {
-  //filters for optiions that are still within the game play and which haven't been yet called more than they are supposed to
-  hints.map((x) =>
-    x.lowerLimit < gameStatus && x.upperLimit > gameStatus
-      ? (x.status = true)
-      : (x.status = false)
-  );
-
-  for (let i = 0; i < hints.length; i++) {
-    if (hints[i].points >= hints[i].limit) {
-      hints[i].status = false;
-    }
-  }
-
-  //creates an array enumerating the viable options each of them times their chance
-  let availableHints = hints.filter((x) => x.status === true);
-
-  for (let i = 0; i < availableHints.length; i++) {
-    for (let j = 0; j < availableHints[i].chance; j++) {
-      options.push(availableHints[i].call);
-    }
-  }
-
-  //one of these instances is being called to provide the hint
-  let rand = Math.floor(Math.random() * options.length);
-  result.push(options[rand](solution, synonymsString));
-
-  //it adds a point for the instance that has been called
-  for (let i = 0; i < availableHints.length; i++) {
-    if (availableHints[i].call.name === options[rand].name) {
-      availableHints[i].points++;
-    }
-  }
-
-  //randomly adds one point to the chances of any instance that will still be played after the game reaches 5
-  let upcomingHints = hints.filter((x) => x.upperLimit > 5);
-  rand = Math.floor(Math.random() * upcomingHints.length);
-  upcomingHints[rand].chance++;
-
-  return hints;
-};
